a3eda1b1e58cb9d834106f58286890140ebca7b5
akaithwar@etouch.net <akaithwar@etouch.net>
2017-11-07 14:13:27 +0530

    Collision Detection

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ var x = canvas.width/2;
 var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
 
 function drawBall(){
 	ctx.beginPath();
-	ctx.arc(x , y, 10, 0, Math.PI*2);
+	ctx.arc(x , y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();	
@@ -19,6 +20,19 @@ function drawBall(){
 function draw() {
 	ctx.clearRect(0,0, canvas.width, canvas.width);
 	drawBall();
+
+	if(y + dy > canvas.height || y + dy < 0 ){
+		dy = -dy;
+	}
+
+	if(x + dx > canvas.width || x + dx < 0 ){
+		dx = -dx;
+	}
+
+
+
+
 	x += dx;
 	y += dy;
+
 }
