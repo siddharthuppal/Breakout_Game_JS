4fb23e6f483e2c110d08600c0c5395bc0938f5d1
akaithwar@etouch.net <akaithwar@etouch.net>
2017-11-07 14:46:21 +0530

    Creating The Paddle

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
-
-setInterval(draw, 10);
-
 var x = canvas.width/2;
 var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
 var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler);
+document.addEventListener("keyup", keyUpHandler);
+
+function keyDownHandler(e){
+	if(e.keyCode == 39){
+		rightPressed = true;
+	}
+	else if(e.keyCode == 37){
+		leftPressed = true;
+	}
+}
+
+function keyUpHandler(e){
+	if(e.keyCode == 39){
+		rightPressed = false;
+	}
+	else if(e.keyCode == 37){
+		leftPressed = false;
+	}
+}
 
 function drawBall(){
 	ctx.beginPath();
@@ -17,9 +40,19 @@ function drawBall(){
 	ctx.closePath();	
 }
 
+function drawPaddle(){
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
+
+
 function draw() {
-	ctx.clearRect(0,0, canvas.width, canvas.width);
+	ctx.clearRect(0,0, canvas.width, canvas.height);
 	drawBall();
+	drawPaddle()
 
 	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius ){
 		dy = -dy;
@@ -29,6 +62,12 @@ function draw() {
 		dx = -dx;
 	}
 
+	if(rightPressed && paddleX < canvas.width-paddleWidth){
+		paddleX += 7;
+	}
+	else if(leftPressed && paddleX > 0){
+		paddleX -= 7;
+	}
 
 
 
@@ -36,3 +75,5 @@ function draw() {
 	y += dy;
 
 }
+
+setInterval(draw, 10);
